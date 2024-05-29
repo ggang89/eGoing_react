108a332bde1a04fc543ba712dc54e2ff0add6d83
ggang89 <ggang89@gmail.com>
2024-05-29 17:05:27 +0900

    이벤트 연결

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">{props.title}</a>
+        <a
+          href="/"
+          onClick={(event) => {
+            //event 객체를 첫번째 파라미터로 호출해준다
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
       </h1>
     </header>
   );
 }
-function Nav() {
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            // 파라미터가 1개일때는 괄호 생략가능
+            // id={t.id}가 onChangeMode가 필요한 id를 넣어주는 것
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+            //함수안에서 a 태그의 id를 받음
+            //event.target =>event를 유발시킨 것(여기서는 a태그)
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+    //a태그도 동적으로 바껴야 하기때문에 {중괄호} 사용
+  }
+
   return (
     <nav>
-      <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-        <li>
-          <a href="/read/3">js</a>
-        </li>
-      </ol>
+      <ol></ol>
     </nav>
   );
 }
@@ -33,10 +56,27 @@ function Article(props) {
   );
 }
 function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is..." },
+    { id: 2, title: "css", body: "css is..." },
+    { id: 3, title: "javascript", body: "javascript is..." },
+  ];
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav></Nav>
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          //onChangeMode라는 props을 갖는다
+          alert("Header");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}
+      ></Nav>
+      {/* "문자열 그대로 전달" {변수 전달} */}
       <Article title="Welcome" body="Hello,WeB"></Article>
       <Article title="HI" body="REACT"></Article>
     </div>
