b89610849c696232c5cabf22c73709e8777ed599
ggang89 <ggang89@gmail.com>
2024-05-29 16:40:58 +0900

    props 연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a href="/">{props.title}</a>
+      </h1>
+    </header>
+  );
+}
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
 function App() {
   return (
-    <div >
-      
+    <div>
+      <Header title="REACT"></Header>
+      <Nav></Nav>
+      <Article title="Welcome" body="Hello,WeB"></Article>
+      <Article title="HI" body="REACT"></Article>
     </div>
   );
 }
